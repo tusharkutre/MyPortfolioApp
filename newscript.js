9b5838b36174d6362519728b862b4ab884cb50a4
tushar kutre <tusharkutre21@gmail.com>
2023-10-29 11:04:31 +0530

    changes done for portfolio

---
+++ newscript.js
@@ -0,0 +1,68 @@
+// onscroll is the event handler which handles some events when some action is performed
+window.onscroll = () =>{
+    // sticky header
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.height = "100vh"
+}
+
+const body = document.body;
+// queryselector for selecting only the classes
+const moon = document.querySelector('.moon');
+const logo = document.querySelector('.logo');
+
+// moon.addEventListener("click",function(e){
+// //  document.body.classList.toggle('white')
+//  document.body.classList.toggle('white');
+//     logo.classList.toggle('textBlack')
+// })
+
+
+// Get references to the select element and the body element
+const selectElement = document.getElementById('themes');
+const bodyElement = document.body;
+
+// Add an event listener to the select element
+selectElement.addEventListener('change', function () {
+    const selectedValue = selectElement.value;
+
+    // Check the selected value and apply the corresponding theme
+    if (selectedValue === 'default') {
+        // Remove any existing class and revert to default styles
+        bodyElement.className = '';
+    } else if (selectedValue === 'dark') {
+        // Apply the dark mode class
+        bodyElement.className = 'dark-mode';
+        logo.classList.toggle('textwhite')
+
+    } else if (selectedValue === 'white') {
+        // Apply the white mode class
+        logo.classList.toggle('textBlack')
+        bodyElement.className = 'white-mode';
+    }
+});
+
+const menu = document.getElementById("menu")
+menu.onclick = function()
+{
+    menu.classList.toggle("openmenu")
+    menu.classList.toggle("active")
+}
+
+function toggleSidebar() {
+    var sidebar = document.querySelector(".sidebar");
+    if (sidebar.style.width === "320px") {
+        sidebar.style.width = "0";
+
+    } else {
+        sidebar.style.width = "320px";
+    }
+    const toggle = document.querySelector('.toggle-button')
+    toggle.classList.toggle('active')
+    toggle.classList.add('apply')
+    
+    // const mobile = document.querySelector('.mobilemode')
+    // mobile.appendChild('apply')
+}
